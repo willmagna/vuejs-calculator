5e2e17608f69c528c8465af7fd861d81e2110360
William Magnabosco <williamlodea@gmail.com>
2023-07-28 13:38:18 -0400

    Setup boilerplate project

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>Vuejs</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./assets/fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: #fff;
+  background: linear-gradient(to right, rgb(83,105,118), rgb(41,46,73));
+}
+
+</style>
